ea44cc864b736b6ffe14b2e03bf9fddd487e436d
Builderb <builderbgamer@gmail.com>
2025-12-27 01:29:25 +0000

    Add datagrid

---
+++ Avalonia.Controls.DataGrid/Extensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Avalonia.Controls
+{
+    internal static class Extensions
+    {
+        internal static Point Translate(this Visual fromElement, Visual toElement, Point fromPoint)
+        {
+            if (fromElement == toElement)
+            {
+                return fromPoint;
+            }
+            else
+            {
+                var transform = fromElement.TransformToVisual(toElement);
+                if (transform.HasValue)
+                    return fromPoint.Transform(transform.Value);
+                else
+                    return fromPoint;
+            }
+        }
+    }
+}
